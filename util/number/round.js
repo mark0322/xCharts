4ac475f413decf4b9ca413cd7ca9abf994914f96
songzilong <songzilong@360.net>
2018-05-11 17:31:36 +0800

    增加 util/number/round.js

---
+++ util/number/round.js
@@ -0,0 +1,13 @@
+/**
+ * 对数组进行四舍五入 (默认四舍五入取整)
+ * @param num 要四舍五入的数字
+ * @param ndigits 要保留的小数位数
+ */
+function round(num, ndigits = 0) {
+  if (ndigits > 0) {
+    return Math.round(num * 10 ** ndigits) / (10 ** ndigits)
+  }
+  return Math.round(num)
+}
+
+export default round
