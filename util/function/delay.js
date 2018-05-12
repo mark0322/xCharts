06458d0807e61ba2d9fdfd27c695a1cc5c8de201
songzilong <songzilong@360.net>
2018-05-12 23:55:42 +0800

    增加 util/function/delay.js

---
+++ util/function/delay.js
@@ -0,0 +1,16 @@
+/**
+ * delay func 函数的执行
+ * 即 lodash 的 _.delay
+ * @param {Function} func The function to delay.
+ * @param {Number} time  The number of milliseconds to delay invocation.
+ * @param {arguments} 可选参
+ */
+
+function delay(func, wait, ...args) {
+  if (typeof func != 'function') {
+    throw new TypeError('第一个参数必须是函数！');
+  }
+  return setTimeout(function() {
+    func.apply(null, args)
+  }, wait)
+}
--- util/function/workFixTime.js
+++ util/function/workFixTime.js
@@ -1,9 +1,9 @@
 /**
- * 模仿 lodash 的 _.before + _.once
+ * 相当于 lodash 的 _.before + _.once
  * @param {Function} func The function to restrict.
  * @param {Number} time 可选参，指定func的执行次数 @default 1
  *
- * eg
+ * @example
  * const show = n => console.log(n)
  * let fn = workFixTime(show, 2)
  * fn('aaa') -> 'aaa'
@@ -11,6 +11,9 @@
  * fn('ccc') -> nothing
  */
 function workFixTime(func, time = 1) {
+  if (typeof func != 'function') {
+    throw new TypeError('第一个参数必须是函数！');
+  }
   let i = -1
   return function() {
     if (++i < time) return func(...arguments)
