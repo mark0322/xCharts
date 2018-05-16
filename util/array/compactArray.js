5ec81af7c0b10c75ed4a974174996f22c2bdd602
songzilong <songzilong@360.net>
2018-05-16 17:51:33 +0800

    clear trail space

--- util/array/compactArray.js
+++ util/array/compactArray.js
@@ -3,7 +3,7 @@
  * @param {Array} arr
  * @param {Function} callback 可选参
  * @return {Array} 被过滤后的数组
- * 
+ *
  * @example
  * compactArray([0, 1, NaN, 2, undefined, 3, null, 4, '', 5])  ->  [0, 1, 2, 3, 4, 5]
  * compactArray([{a:1}, {a:2, b:22}, {a:3, b:33}], d => d.b)  ->  [22, 33]
