914fefecd9dd191d1f667e2dbef30de073c0fe75
songzilong <songzilong@360.net>
2018-05-30 10:55:37 +0800

    增加 util/object/each.js

---
+++ util/object/each.js
@@ -0,0 +1,18 @@
+/**
+ * 如Array.prototype.forEach()方法, 遍历 object 每个属性
+ * @param {Object} obj 被遍历的对象
+ * @param {Function} callback fn有三参数： key, value, self
+ * 
+ * @example
+ * let o = { a: 1, b: 2}
+ * each(o, (key, value, self) => console.log(key, value, self))
+ * -> a 1 {a: 1, b: 2}
+ * -> b 2 {a: 1, b: 2}
+ */
+function each(obj, callback) {
+  for (let key of Object.keys(obj)) {
+    callback.call(null, key, obj[key], obj)
+  }
+}
+
+export default each
