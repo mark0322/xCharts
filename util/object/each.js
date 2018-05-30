868a2554a322e4bf264ac7713e1126c616d6c8da
songzilong <songzilong@360.net>
2018-05-30 16:01:48 +0800

    each.js 注释完善

--- util/object/each.js
+++ util/object/each.js
@@ -5,10 +5,18 @@
  * @param {Function} callback 回掉函数可有三参数： key, value, self
  * 
  * @example
+ * eg1:
  * const o = {a: 1, b: 2}
  * each(o, (key, value, self) => console.log(key, value, self))
  * -> a 1 {a: 1, b: 2}
  * -> b 2 {a: 1, b: 2}
+ * 
+ * eg2:
+ * each([ 52, 97 ], function( index, value ) {
+ *   alert( index + ": " + value );
+ * });
+ * -> 0: 52 
+ * -> 1: 97
  */
 function each(obj, callback) {
   for (let key of Object.keys(obj)) {
